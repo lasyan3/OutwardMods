e0c3f8c93dc24e40ae3c168f109b23d021f88315
lasyan3 <las@as.com>
2020-04-24 14:09:00 +0200

    MoreGatherableLoot: Compatible with 21th April update.

---
+++ MoreGatherableLoot/Hooks/ItemDropper_GenerateItem.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+
+namespace MoreGatherableLoot
+{
+    [HarmonyPatch(typeof(ItemDropper), "GenerateItem")]
+    public class ItemDropper_GenerateItem
+    {
+        [HarmonyPrefix]
+        public static bool GenerateItem(ItemContainer _container, BasicItemDrop _itemDrop, ref int _spawnAmount)
+        {
+            try
+            {
+                _itemDrop.DroppedItem.InitCachedInfos();
+                //MoreGatherableLoot.MyLogger.LogDebug($"{_itemDrop.DroppedItem.DisplayName}");
+                if (_container.GetType() == typeof(Gatherable)
+                    && (_itemDrop.DroppedItem.IsFood || _itemDrop.DroppedItem.IsIngredient)
+                    && !_itemDrop.DroppedItem.IsDrink
+                    && !_itemDrop.DroppedItem.IsEquippable
+                    && !_itemDrop.DroppedItem.IsDeployable
+                    && _itemDrop.MaxDropCount < MoreGatherableLoot.Settings.AmountMax)
+                {
+                    int minDrop = _itemDrop.MinDropCount * (MoreGatherableLoot.Settings.AlwaysMax ? MoreGatherableLoot.Settings.AmountMax : MoreGatherableLoot.Settings.AmountMin);
+                    int maxDrop = _itemDrop.MaxDropCount * MoreGatherableLoot.Settings.AmountMax;
+                    if (_itemDrop.MaxDropCount > 1 && _itemDrop.MaxDropCount < MoreGatherableLoot.Settings.AmountMax)
+                    { // If already multiple quantities, increase instead of multiply
+                        minDrop = MoreGatherableLoot.Settings.AlwaysMax ? MoreGatherableLoot.Settings.AmountMax : MoreGatherableLoot.Settings.AmountMin;
+                        maxDrop = MoreGatherableLoot.Settings.AmountMax;
+                    }
+                    // Increase count of items on specific resources (like champignons !)
+                    //MoreGatherableLoot.MyLogger.LogDebug($"{_container.GetType().Name}: {_container.name.Split(new char[] { '_' })[1]}");
+                    //MoreGatherableLoot.MyLogger.LogDebug($"{_itemDrop.DroppedItem.name.Split(new char[] { '_' })[1]} ({_itemDrop.MinDropCount} - {_itemDrop.MaxDropCount})");
+                    _spawnAmount = UnityEngine.Random.Range(minDrop, maxDrop + 1);
+                    //MoreGatherableLoot.MyLogger.LogDebug($"{_itemDrop.DroppedItem.DisplayName}={_spawnAmount} ({minDrop} - {maxDrop})");
+                    MoreGatherableLoot.MyLogger.LogDebug($"\t|- New amount={_spawnAmount} ({minDrop} - {maxDrop})");
+                }
+            }
+            catch (Exception ex)
+            {
+                MoreGatherableLoot.MyLogger.LogError(ex.Message);
+            }
+            return true;
+        }
+    }
+}
--- MoreGatherableLoot/MoreGatherableLoot.cs
+++ MoreGatherableLoot/MoreGatherableLoot.cs
@@ -1,87 +1,41 @@
-﻿//using ODebug;
-using Partiality.Modloader;
+﻿using BepInEx;
 using System;
 using System.IO;
 using UnityEngine;
+using HarmonyLib;
+using BepInEx.Logging;
 
 namespace MoreGatherableLoot
 {
-    class GameData
+    public class GameData
     {
         public int AmountMin;
         public int AmountMax;
         public bool AlwaysMax;
     }
 
-    public class MoreGatherableLoot : PartialityMod
+    [BepInPlugin(ID, NAME, VERSION)]
+    public class MoreGatherableLoot : BaseUnityPlugin
     {
-        private GameData data;
-        private readonly string m_modName = "MoreGatherableLoot";
+        const string ID = "com.lasyan3.moregatherableloot";
+        const string NAME = "MoreGatherableLoot";
+        const string VERSION = "1.0.3";
 
-        public MoreGatherableLoot()
-        {
-            this.ModID = m_modName;
-            this.Version = "1.0.2";
-            //this.loadPriority = 0;
-            this.author = "lasyan3";
-        }
+        public static GameData Settings;
+        public static ManualLogSource MyLogger = BepInEx.Logging.Logger.CreateLogSource(NAME);
 
-        public override void Init()
-        {
-            base.Init();
-            data = LoadSettings();
-        }
-
-        public override void OnLoad() { base.OnLoad(); }
-
-        public override void OnDisable()
-        {
-            base.OnDisable();
-
-            On.ItemDropper.GenerateItem -= ItemDropper_GenerateItem;
-        }
-
-        public override void OnEnable()
-        {
-            base.OnEnable();
-
-            On.ItemDropper.GenerateItem += ItemDropper_GenerateItem;
-        }
-
-        private void ItemDropper_GenerateItem(On.ItemDropper.orig_GenerateItem orig, ItemDropper self, ItemContainer _container, BasicItemDrop _itemDrop, int _spawnAmount)
+        internal void Awake()
         {
             try
             {
-                _itemDrop.DroppedItem.InitCachedInfos();
-                if (_container.GetType() == typeof(Gatherable)
-                    && (_itemDrop.DroppedItem.IsFood || _itemDrop.DroppedItem.IsIngredient)
-                    && !_itemDrop.DroppedItem.IsDrink
-                    && !_itemDrop.DroppedItem.IsEquippable
-                    && !_itemDrop.DroppedItem.IsDeployable
-                    && _itemDrop.MaxDropCount < data.AmountMax)
-                {
-                    int minDrop = _itemDrop.MinDropCount * (data.AlwaysMax ? data.AmountMax : data.AmountMin);
-                    int maxDrop = _itemDrop.MaxDropCount * data.AmountMax;
-                    if (_itemDrop.MaxDropCount > 1 && _itemDrop.MaxDropCount < data.AmountMax)
-                    { // If already multiple quantities, increase instead of multiply
-                        minDrop = data.AlwaysMax ? data.AmountMax : data.AmountMin;
-                        maxDrop = data.AmountMax;
-                    }
-                    // Increase count of items on specific resources (like champignons !)
-                    //OLogger.Log($"{_container.GetType().Name}: {_container.name.Split(new char[] { '_' })[1]}");
-                    //OLogger.Log($"{_itemDrop.DroppedItem.name.Split(new char[] { '_' })[1]} ({_itemDrop.MinDropCount} - {_itemDrop.MaxDropCount})");
-                    _spawnAmount = UnityEngine.Random.Range(minDrop, maxDrop + 1);
-                    //OLogger.Log($"{_itemDrop.DroppedItem.DisplayName}={_spawnAmount} ({minDrop} - {maxDrop})", "ffffffff", _modName);
-                }
+                var harmony = new Harmony(ID);
+                harmony.PatchAll();
+                Settings = LoadSettings();
+                MyLogger.LogDebug("Awaken");
             }
             catch (Exception ex)
             {
-                //OLogger.Error("ItemDropper_GenerateItem: " + ex.Message, _modName);
-                Debug.Log($"[{m_modName}] ItemDropper_GenerateItem: {ex.Message}");
-            }
-            finally
-            {
-                orig.Invoke(self, _container, _itemDrop, _spawnAmount);
+                MyLogger.LogError(ex.Message);
             }
         }
 
@@ -89,14 +43,14 @@ namespace MoreGatherableLoot
         {
             try
             {
-                using (StreamReader streamReader = new StreamReader($"mods/{m_modName}Config.json"))
+                using (StreamReader streamReader = new StreamReader($"BepInEx/config/{NAME}Config.json"))
                 {
                     return JsonUtility.FromJson<GameData>(streamReader.ReadToEnd());
                 }
             }
             catch (Exception ex)
             {
-                Debug.Log($"[{m_modName}] LoadSettings: {ex.Message}");
+                MyLogger.LogError(ex.Message);
             }
             return null;
         }
