254c2f9a49e8ec356c5a350877790b0675f84e80
lasyan3 <las@as.com>
2020-04-25 10:42:20 +0200

    New project: CustomKeybindingsBepInEx

---
+++ CustomKeybindingsBepInEx/Hooks/ControlMappingPanel_InitSections.cs
@@ -0,0 +1,74 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using Rewired;
+using UnityEngine;
+using static CustomKeybindings;
+
+namespace Hooks
+{
+    [HarmonyPatch(typeof(ControlMappingPanel), "InitSections")]
+    public class ControlMappingPanel_InitSections
+    {
+        static ManualLogSource MyLogger = BepInEx.Logging.Logger.CreateLogSource("InitSections");
+
+        [HarmonyPrefix]
+        public static bool InitSections( ControllerMap _controllerMap)
+        {
+
+            // Loop through our custom actions we added via Rewired
+            foreach (int myActionId in CustomKeybindings.myCustomActionIds.Keys)
+            {
+
+                // The info that the user specified for this action
+                CustomKeybindings.InputActionDescription myActionDescription = CustomKeybindings.myCustomActionIds[myActionId];
+
+                // There are separate keybinding maps for keyboard, mouse, & controllers
+                // We only add our action-to-element mappings to the keybind maps that make sense
+                // For example, if you are adding a key that doesn't make sense to have on a controller,
+                // then skip when _controllerMap is JoystickMap
+                //
+                // (Optional)
+                // You can check if this method is being called for the Keyboard/Mouse bindings panel or
+                // the Controller bindings panel, but I prefer to check the class of the _controllerMap
+                //   if (self.ControllerType == ControlMappingPanel.ControlType.Keyboard) {
+                //
+
+                bool shouldLog = true;
+                if (shouldLog)
+                {
+                    MyLogger.LogDebug("_controllerMap is keyboard or mouse: " + (_controllerMap is KeyboardMap || _controllerMap is MouseMap));
+                    MyLogger.LogDebug("_controllerMap is joystick: " + (_controllerMap is JoystickMap));
+                    MyLogger.LogDebug("_controllerMap.categoryId: " + _controllerMap.categoryId);
+                    MyLogger.LogDebug("action is keyboard: " + (myActionDescription.controlType == ControlType.Keyboard));
+                    MyLogger.LogDebug("action is gamepad: " + (myActionDescription.controlType == ControlType.Gamepad));
+                    MyLogger.LogDebug("action is both: " + (myActionDescription.controlType == ControlType.Both));
+                    MyLogger.LogDebug("action.sectionId: " + myActionDescription.sectionId);
+                }
+
+                // If the controller map's control type does not match our action
+                if (!(myActionDescription.controlType == ControlType.Keyboard && (_controllerMap is KeyboardMap || _controllerMap is MouseMap) ||
+                      myActionDescription.controlType == ControlType.Gamepad && (_controllerMap is JoystickMap) ||
+                      myActionDescription.controlType == ControlType.Both))
+                {
+                    // Then skip to next action
+                    continue;
+                }
+
+                // If the categoryId of this controller map does not match our action's
+                if (_controllerMap.categoryId != myActionDescription.sectionId)
+                {
+                    // Skip to next action
+                    continue;
+                }
+
+                // If we pass the tests, create & add the action-to-element map for this particular action
+                _controllerMap.CreateElementMap(myActionId, Pole.Positive, KeyCode.None, ModifierKeyFlags.None);
+
+                // Continue the loop...
+            }
+
+            // We're done here. Call original implementation
+            return true;
+        }
+    }
+}
---
+++ CustomKeybindingsBepInEx/Hooks/LocalizationManager_StartLoading.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Rewired;
+using System.Collections.Generic;
+using System.Reflection;
+using static CustomKeybindings;
+
+namespace Hooks
+{
+    [HarmonyPatch(typeof(LocalizationManager), "StartLoading")]
+    public class LocalizationManager_StartLoading
+    {
+        [HarmonyPostfix]
+        public static void StartLoading(LocalizationManager __instance)
+        {
+            // Nab the localization dictionary that's used for keybind localization
+            Dictionary<string, string> m_generalLocalization = (Dictionary<string, string>)__instance.GetType().GetField("m_generalLocalization", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+
+            // Go through the added actions and use the user-created action descriptions to name them
+            foreach (int myActionId in CustomKeybindings.myCustomActionIds.Keys)
+            {
+                InputAction myAction = ReInput.mapping.GetAction(myActionId);
+                InputActionDescription myActionDescription = CustomKeybindings.myCustomActionIds[myActionId];
+
+                // The first string is the technical name of the action, while the second string is what you want to display in the bindings menu
+                m_generalLocalization.Add("InputAction_" + myAction.name, myActionDescription.name);
+            }
+        }
+    }
+}
